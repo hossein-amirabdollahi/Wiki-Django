899823fdac208574876c23fb467e093d922fda0c
Hossein Amirabdollahi <hosseinamirabdollahi8@gmail.com>
2024-11-04 17:49:49 +0330

    add edit and random features

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import markdown
-
 from . import util
+import random
 
 def convert_md_to_html(title):
     content = util.get_entry(title)
@@ -66,3 +66,32 @@ def new_page(request):
                 "title": title,
                 "content": html_content
             })
+        
+def edit(request):
+    if request.method == "POST":
+        title = request.POST["entry_title"]
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
+        })
+
+def save_edit(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = convert_md_to_html(title)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+    
+def rand(request):
+    all_entries = util.list_entries()
+    rand_entry = random.choice(all_entries)
+    html_content = convert_md_to_html(rand_entry)
+    return render(request, "encyclopedia/entry.html", {
+        "title": rand_entry,
+        "content": html_content
+    })
